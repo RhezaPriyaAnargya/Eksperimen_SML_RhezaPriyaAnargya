c680b7ae3eacab4490f56198e7674f04a666b825
RhezaPriyaAnargya <rezhaanarga@gmail.com>
2025-12-21 19:50:32 +0700

    Fix

--- Membangun_model/modelling.py
+++ Membangun_model/modelling.py
@@ -1,11 +1,10 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.metrics import accuracy_score
 import mlflow
 import mlflow.sklearn
 
-# Load data
 df = pd.read_csv("namadataset_preprocessing/telco_preprocessed.csv")
 
 X = df.drop("target", axis=1)
@@ -17,16 +16,24 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 mlflow.set_experiment("Telco-Churn-Basic")
 
-with mlflow.start_run():
-    mlflow.sklearn.autolog()
+# Siapkan input example
+input_example = X_train[:5]
+
+with mlflow.start_run(run_name="logistic_regression_model"):
+    mlflow.autolog()
 
     model = LogisticRegression(max_iter=1000)
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
-
     acc = accuracy_score(y_test, y_pred)
-    prec = precision_score(y_test, y_pred)
-    rec = recall_score(y_test, y_pred)
-
+    
+    mlflow.log_metric("accuracy", acc)
     print("Accuracy:", acc)
+    
+    # Log model
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="model",
+        input_example=input_example
+    )
